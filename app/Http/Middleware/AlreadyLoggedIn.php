be3fe60d3f13d529bb55d7e3c276e7125a7f6810
Omar Fathy Helmy <93728655+Omarfathy50@users.noreply.github.com>
2022-11-05 15:43:37 +0100

    Update AlreadyLoggedIn.php

--- app/Http/Middleware/AlreadyLoggedIn.php
+++ app/Http/Middleware/AlreadyLoggedIn.php
@@ -16,7 +16,7 @@ class AlreadyLoggedIn
     public function handle($request, Closure $next)
     {
         if(Session()->has('loginId')  && (url('login')==$request->url())){
-            return redirect('login')->with('error', 'You have to login first!');
+            return back();
         }
         return $next($request);
     }
